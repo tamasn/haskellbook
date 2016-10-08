14d4d6c24592895510f26f0079264941c40a6f76
Tamas Neltz <tamas.neltz@gmail.com>
2016-10-07 22:36:28 -0400

    chapter 27 exercises

--- hbch27/src/CoreDump.hs
+++ hbch27/src/CoreDump.hs
@@ -1,9 +1,26 @@
 module CoreDump where
 
-discriminatory :: Bool -> Int
+data Test = A Test2 | B Test2 deriving (Show)
 
+data Test2 = C Int | D Int deriving (Show)
+
+
+discriminatory :: Bool -> Int
 discriminatory b =
-  case b of
+  let x = undefined
+  in case x `seq` b of
     False -> 0
     True -> 1
 
+forceNothing :: Test -> Int
+forceNothing _ = 0
+
+forceTest :: Test -> Int
+forceTest (A _) = 1
+forceTest (B _) = 2
+
+forceTest2 :: Test -> Int
+forceTest2 (A (C i)) = i
+forceTest2 (A (D i)) = i
+forceTest2 (B (C i)) = i
+forceTest2 (B (D i)) = i
