17b8806847a7e43631f22a8ee05b2f916c6bd051
Tamas Neltz <tamas.neltz@gmail.com>
2016-10-09 16:58:29 -0400

    test cafs

--- hbch28/src/profilingCafs.hs
+++ hbch28/src/profilingCafs.hs
@@ -1,13 +1,13 @@
 module ProfilingCafs (mainCaf) where
 
-incdInts :: [Integer]
-incdInts = map (+1) [1..]
+incdInts :: [Integer] -> [Integer]
+incdInts x = map (+1) x 
 
 mainCaf :: IO ()
 mainCaf = do
-  print (incdInts !! 1000)
-  print (incdInts !! 9001)
-  print (incdInts !! 90010)
-  print (incdInts !! 9001000)
-  print (incdInts !! 9501000)
-  print (incdInts !! 9901000)
+  print (incdInts [1..] !! 1000)
+  print (incdInts [1..] !! 9001)
+  print (incdInts [1..] !! 90010)
+  print (incdInts [1..] !! 9001000)
+  print (incdInts [1..] !! 9501000)
+  print (incdInts [1..] !! 9901000)
